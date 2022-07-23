546966271653da7c358b9d46a2d658e993f365a3
Alexander Profeit <alex@profeit.com>
2022-07-23 03:37:46 +0100

    start building file picker

--- app/javascript/components/Upload.js
+++ app/javascript/components/Upload.js
@@ -1,21 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import * as Icons from "react-feather"
+import { useFilePicker } from "use-file-picker";
 
-class Upload extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <div className="upload-container">
-          <div className="upload-btn" onClick={() => { alert("hello world") }}>
-            <Icons.Upload size={128} strokeWidth={0.4} />
-            <div>
-              <a className="upload-text">Upload image</a>
-            </div>
+export default function Upload() {
+  const [uploading, setUploading] = useState(false);
+
+  const [openFilePicker, { filesContent }] = useFilePicker({
+    accept: "image/*",
+    maxFileSize: 20,
+    multiple: false,
+    readAs: "DataURL",
+  });
+
+  if (filesContent.length === 1) {
+    console.log('got file');
+  }
+
+  const uploadOnClick = () => {
+    setUploading(true);
+    openFilePicker();
+  }
+
+  return (
+    <React.Fragment>
+      <div className="upload-container">
+        <div className="upload-btn" onClick={() => uploadOnClick()}>
+          <Icons.Upload size={128} strokeWidth={0.4} />
+          <div>
+            <span className="upload-text">{ uploading ? "Uploading..." : "Upload image" }</span>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
+      </div>
+    </React.Fragment>
+  )
 }
-
-export default Upload
