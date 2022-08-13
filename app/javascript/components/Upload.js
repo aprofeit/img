a8ec4d60a83e09eca770509737f002fd6d132f4e
Alexander Profeit <alex@profeit.com>
2022-08-13 03:40:48 +0100

    remove react-router-dom

--- app/javascript/components/Upload.js
+++ app/javascript/components/Upload.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
 import * as Icons from "react-feather"
 import { useFilePicker } from "use-file-picker"
 import axios from 'axios';
